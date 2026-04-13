b361e8ee16de5c796f110ab5d2a27e621d2442de
Atharv-design <atharvgupta790@gmail.com>
2026-04-13 18:43:01 +0530

    fix requirements

--- app/__init__.py
+++ app/__init__.py
@@ -3,34 +3,29 @@ from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+# extensions
 db = SQLAlchemy()
 login_manager = LoginManager()
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # Initialize extensions
+    # init extensions
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
 
-    # Import models (important for login loader)
-    from app.models.user_model import User
-
-    # Login Loader
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
-
-    # Import routes
+    # ======================
+    # IMPORT & REGISTER ROUTES
+    # ======================
     from app.routes.auth_routes import auth_bp
     from app.routes.resume_routes import resume_bp
     from app.routes.job_routes import job_bp
     from app.routes.interview_routes import interview_bp
     from app.routes.roadmap_routes import roadmap_bp
 
-    # Register blueprints
     app.register_blueprint(auth_bp)
     app.register_blueprint(resume_bp)
     app.register_blueprint(job_bp)
