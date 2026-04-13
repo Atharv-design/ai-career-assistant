5c0a6fb3a30f4d713f46ec95e20202c3ea0206c8
Atharv-design <atharvgupta790@gmail.com>
2026-04-13 19:20:07 +0530

    full UI + dashboard setup

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,41 +1,15 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from app.models.user_model import User
-from app import db
-from flask_login import login_user, logout_user, login_required
+from flask import Blueprint, render_template
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint("auth", __name__)
 
-@auth_bp.route('/')
+@auth_bp.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@auth_bp.route('/register', methods=['GET','POST'])
-def register():
-    if request.method == 'POST':
-        user = User(
-            username=request.form['username'],
-            password=request.form['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        return redirect('/login')
-    return render_template('login.html')
-
-@auth_bp.route('/login', methods=['GET','POST'])
+@auth_bp.route("/login")
 def login():
-    if request.method == 'POST':
-        user = User.query.filter_by(username=request.form['username']).first()
-        if user and user.password == request.form['password']:
-            login_user(user)
-            return redirect('/dashboard')
-    return render_template('login.html')
+    return render_template("login.html")
 
-@auth_bp.route('/dashboard')
-@login_required
+@auth_bp.route("/dashboard")
 def dashboard():
-    return render_template('dashboard.html')
-
-@auth_bp.route('/logout')
-def logout():
-    logout_user()
-    return redirect('/')
+    return render_template("dashboard.html")
